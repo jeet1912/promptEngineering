21dc982e2f2620a629a5c9a093c648bdde0d880a
jeet1912 <sahdevjeet@gmail.com>
2023-04-07 19:28:56 +0530

    implemented ui using streamlit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import openai
 import requests
 import json
+import streamlit as st
 
 openai.api_key = "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"
 
@@ -12,11 +13,6 @@ def BasicGeneration(userPrompt):
         "content" : userPrompt
         }])
     return completion.choices[0].message.content
-"""
-prompt = "analyze last 7 days bitcoin prices"
-response = BasicGeneration(prompt)
-print(response)
-"""
 
 def GetCryptoPrices():
 
@@ -46,19 +42,25 @@ def GetCryptoPrices():
     
     return priceList
 
-bitcoinPrices = GetCryptoPrices()
 
-chatGPTPrompt = f"""I will provide you with a list of bitcoin prices for the last seven days. Could you provide me with a technical analysis of Bitcoin based on these prices. Here is what I want:
-Price Overview,
-Moving Averages,
-Relative Strength Index (RSI),
-Moving Average Convergence Divergence (MACD),
-Advice and Suggestion,
-Do I buy or sell?
-Please be as detailed, and explain in a way any beginner can understand.
-Here is the price list: {bitcoinPrices}
-"""
+st.title("Bitcoin Analyzer using ChatGPT")
 
-analysis = BasicGeneration(chatGPTPrompt)
+if st.button("Analyze"):
+    with st.spinner('Getting Bitcoin Prices...'):
+        bitcoinPrices = GetCryptoPrices()
+        st.success("Done!")
+    with st.spinner("Analyzing Bitcoin Prices..."):
+        chatGPTPrompt = f"""I will provide you with a list of bitcoin prices for the last seven days. Could you provide me with a technical analysis of Bitcoin based on these prices. Here is what I want:
+                            Price Overview,
+                            Moving Averages,
+                            Relative Strength Index (RSI),
+                            Moving Average Convergence Divergence (MACD),
+                            Advice and Suggestion,
+                            Do I buy or sell?
+                            Please be as detailed, and explain in a way any beginner can understand.
+                            Here is the price list: {bitcoinPrices}
+                            """
+        analysis = BasicGeneration(chatGPTPrompt)
+        st.text_area("Analysis", analysis, height=500)
+        st.success('Done!')
 
-print(analysis)
