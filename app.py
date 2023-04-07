e7f4d22e62461d8e41328d6ed4a86d3ac6640c4a
jeet1912 <sahdevjeet@gmail.com>
2023-04-07 18:59:43 +0530

    get crypto prices

--- app.py
+++ app.py
@@ -18,3 +18,31 @@ response = BasicGeneration(prompt)
 print(response)
 """
 
+def getCryptoPrices():
+
+    # define api endpoint and query params
+    url = "https://coinranking1.p.rapidapi.com/coin/Qwsogvtv82FCd/history"
+    querystring = {"referenceCurrencyUuid":"yhjMzLPhuIDl","timePeriod":"7d"}
+
+    # define request headers with api key and host
+    headers = {
+        "X-RapidAPI-Key": "7f4d53cc47msh214301e0e9baec0p1694ddjsna8c84cadc4fa",
+        "X-RapidAPI-Host": "coinranking1.p.rapidapi.com"
+    }
+
+    # send a get request to API endpoint with query params and headers
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    # parse response as a JSON object
+    JSONResult = json.load(response.text)
+
+    history = JSONResult["data"]["history"]
+    prices = []
+
+    for value in history:
+        prices.append(value["price"])
+    
+    priceList = ','.join(prices)
+    
+    return priceList
+
